e859e28952c1ff0c3b721ab28ef9c73098b4f8ae
Arthur AILLET <arthur.aillet@smile.fr>
2026-04-13 17:18:20 +0200

    feat: Orchestrator implementation to cpp, fix Makefile to match

---
+++ src/Orchestrator.cpp
@@ -0,0 +1,48 @@
+#include "Orchestrator.hpp"
+#include "ColorScene.hpp"
+#include "IncrementalObserver.hpp"
+
+Orchestrator::Orchestrator(QObject *parent) : QObject(parent) {
+  observers.push_back(std::make_shared<IncrementalObserver>());
+
+  scenes.push_back(std::make_unique<ColorScene>(this));
+
+  for (auto &scene : scenes) {
+    const QMetaObject *metaObject = scene->metaObject();
+
+    for (int i = metaObject->propertyOffset(); i < metaObject->propertyCount(); ++i) {
+      driver_mapping.insert(std::pair("intensity", metaObject->property(i)));
+    }
+  }
+  SelectObserver(0);
+}
+
+void Orchestrator::getDriverValue(std::string name, QVariant value) {
+  auto range = driver_mapping.equal_range(name);
+
+  for (auto it = range.first; it != range.second; ++it) {
+    auto [_, scene_property] = *it;
+
+    scenes[0]->setProperty(scene_property.name(), value);
+  }
+}
+
+void Orchestrator::DeselectObserver() {
+  if (selectedObserver != nullptr) {
+    selectedObserver->exit();
+    selectedObserver->disconnect(SIGNAL(setDriverValue(std::string, QVariant)), this,
+                                 SLOT(getDriverValue(std::string, QVariant)));
+  }
+}
+
+void Orchestrator::SelectObserver(size_t index) {
+  if (index < observers.size() && observers[index].unique()) {
+    DeselectObserver();
+
+    selectedObserver = observers[index];
+
+    selectedObserver->start();
+    QObject::connect(&*selectedObserver, SIGNAL(setDriverValue(std::string, QVariant)), this,
+                     SLOT(getDriverValue(std::string, QVariant)));
+  }
+}
--- src/Orchestrator.hpp
+++ src/Orchestrator.hpp
@@ -2,66 +2,23 @@
 
 #include <QMetaProperty>
 #include <QObject>
-#include <cstddef>
-#include <memory>
 
 #include "Observer.hpp"
 #include "Scene.hpp"
-#include "observers/IncrementalObserver.hpp"
-#include "scenes/ColorScene.hpp"
 
 class Orchestrator : public QObject {
   Q_OBJECT
+  private:
   std::vector<std::unique_ptr<Scene>> scenes;
   std::vector<std::shared_ptr<Observer>> observers;
   std::unordered_multimap<std::string, QMetaProperty> driver_mapping;
   std::shared_ptr<Observer> selectedObserver = nullptr;
 
   public slots:
-  void getDriverValue(std::string name, QVariant value) {
-    auto range = driver_mapping.equal_range(name);
-    for (auto it = range.first; it != range.second; ++it) {
-      auto [observerKey, scene_property] = *it;
-      auto a = scenes[0]->property(scene_property.name()).toInt();
-      qDebug() << "YES!" << observerKey << "  " << scene_property.name() << " : " << value << "  "
-               << a;
-      scenes[0]->setProperty(scene_property.name(), value);
-    }
-  }
+  void getDriverValue(std::string name, QVariant value);
 
   public:
-  void DeselectObserver() {
-    if (selectedObserver != nullptr) {
-      selectedObserver->exit();
-      selectedObserver->disconnect(SIGNAL(setDriverValue(std::string, QVariant)), this,
-                                   SLOT(getDriverValue(std::string, QVariant)));
-    }
-  }
-
-  void SelectObserver(size_t index) {
-    if (index < observers.size() && observers[index].unique()) {
-      DeselectObserver();
-
-      selectedObserver = observers[index];
-
-      selectedObserver->start();
-      QObject::connect(&*selectedObserver, SIGNAL(setDriverValue(std::string, QVariant)), this,
-                       SLOT(getDriverValue(std::string, QVariant)));
-    }
-  }
-
-  Orchestrator(QObject *parent) : QObject(parent) {
-    observers.push_back(std::make_shared<IncrementalObserver>());
-
-    scenes.push_back(std::make_unique<ColorScene>(this));
-
-    for (auto &scene : scenes) {
-      const QMetaObject *metaObject = scene->metaObject();
-
-      for (int i = metaObject->propertyOffset(); i < metaObject->propertyCount(); ++i) {
-        driver_mapping.insert(std::pair("intensity", metaObject->property(i)));
-      }
-    }
-    SelectObserver(0);
-  }
+  void DeselectObserver();
+  void SelectObserver(size_t index);
+  Orchestrator(QObject *parent = nullptr);
 };
