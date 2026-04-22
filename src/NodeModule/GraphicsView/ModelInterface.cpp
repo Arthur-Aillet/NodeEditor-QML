bcebe7cd563cb682986c54267c33a785b9e8be87
Arthur AILLET <arthur.aillet@smile.fr>
2026-04-22 09:59:42 +0200

    feat: moved ModelInterface implementations to cpp

---
+++ src/NodeModule/GraphicsView/ModelInterface.cpp
@@ -0,0 +1,31 @@
+#include "ModelInterface.hpp"
+
+ModelInterface::ModelInterface(QtNodes::AbstractGraphModel &_graphModel)
+    : QObject(nullptr), graphModel(_graphModel) {
+
+  connect(&graphModel, SIGNAL(connectionCreated(ConnectionId)), this,
+          SIGNAL(connectionCreated(ConnectionId)));
+  connect(&graphModel, SIGNAL(connectionDeleted(ConnectionId)), this,
+          SIGNAL(connectionDeleted(ConnectionId)));
+  connect(&graphModel, SIGNAL(nodeCreated(NodeId)), this, SIGNAL(nodeCreated(NodeId)));
+  connect(&graphModel, SIGNAL(nodeDeleted(NodeId)), this, SIGNAL(nodeDeleted(NodeId)));
+  connect(&graphModel, SIGNAL(nodeUpdated(NodeId)), this, SIGNAL(nodeUpdated(NodeId)));
+  connect(&graphModel, SIGNAL(nodeFlagsUpdated(NodeId)), this, SIGNAL(nodeFlagsUpdated(NodeId)));
+  connect(&graphModel, SIGNAL(nodePositionUpdated(NodeId)), this,
+          SIGNAL(nodePositionUpdated(NodeId)));
+  connect(&graphModel, SIGNAL(modelReset()), this, SIGNAL(modelReset()));
+}
+
+ModelInterface *ModelInterface::create(QQmlEngine *, QJSEngine *engine) {
+  if (instance == nullptr) {
+    qCritical() << "Model Interface never setup.";
+  } else {
+    QJSEngine::setObjectOwnership(instance, QJSEngine::CppOwnership);
+  }
+  return instance;
+}
+
+ModelInterface *ModelInterface::init(QtNodes::AbstractGraphModel &_graphModel) {
+  instance = new ModelInterface(_graphModel);
+  return instance;
+};
--- src/NodeModule/GraphicsView/ModelInterface.hpp
+++ src/NodeModule/GraphicsView/ModelInterface.hpp
@@ -1,18 +1,11 @@
 #pragma once
 
-#include "NodeModel.hpp"
-#include "QtNodes/internal/BasicGraphicsScene.hpp"
-#include "QtNodes/internal/DataFlowGraphModel.hpp"
-#include "QtNodes/internal/QmlWrapper.hpp"
-#include "ValueNodeModel.hpp"
-#include <QDebug>
 #include <QObject>
 #include <QtNodes/AbstractGraphModel>
 #include <QtNodes/BasicGraphicsScene>
 #include <QtNodes/DataFlowGraphicsScene>
 #include <qdebug.h>
 #include <qjsengine.h>
-#include <qlogging.h>
 #include <qloggingcategory.h>
 #include <qobject.h>
 #include <qqmlintegration.h>
@@ -31,35 +24,11 @@ class ModelInterface : public QObject {
 
   inline static ModelInterface *instance = nullptr;
 
-  static ModelInterface *create(QQmlEngine *, QJSEngine *engine) {
-    if (instance == nullptr) {
-      qCritical() << "Model Interface never setup.";
-    } else {
-      QJSEngine::setObjectOwnership(instance, QJSEngine::CppOwnership);
-    }
-    return instance;
-  }
-
-  static ModelInterface *init(QtNodes::AbstractGraphModel &_graphModel) {
-    instance = new ModelInterface(_graphModel);
-    return instance;
-  };
+  static ModelInterface *create(QQmlEngine *, QJSEngine *engine);
+  static ModelInterface *init(QtNodes::AbstractGraphModel &_graphModel);
 
   protected:
-  ModelInterface(QtNodes::AbstractGraphModel &_graphModel)
-      : QObject(nullptr), graphModel(_graphModel) {
-    connect(&graphModel, SIGNAL(connectionCreated(ConnectionId)), this,
-            SIGNAL(connectionCreated(ConnectionId)));
-    connect(&graphModel, SIGNAL(connectionDeleted(ConnectionId)), this,
-            SIGNAL(connectionDeleted(ConnectionId)));
-    connect(&graphModel, SIGNAL(nodeCreated(NodeId)), this, SIGNAL(nodeCreated(NodeId)));
-    connect(&graphModel, SIGNAL(nodeDeleted(NodeId)), this, SIGNAL(nodeDeleted(NodeId)));
-    connect(&graphModel, SIGNAL(nodeUpdated(NodeId)), this, SIGNAL(nodeUpdated(NodeId)));
-    connect(&graphModel, SIGNAL(nodeFlagsUpdated(NodeId)), this, SIGNAL(nodeFlagsUpdated(NodeId)));
-    connect(&graphModel, SIGNAL(nodePositionUpdated(NodeId)), this,
-            SIGNAL(nodePositionUpdated(NodeId)));
-    connect(&graphModel, SIGNAL(modelReset()), this, SIGNAL(modelReset()));
-  }
+  ModelInterface(QtNodes::AbstractGraphModel &_graphModel);
 
   public:
   Q_SIGNALS:
