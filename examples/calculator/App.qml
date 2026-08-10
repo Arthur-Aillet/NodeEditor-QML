f3ccc89357c0267bce9fd3832c5657f5ab62d67c
Arthur AILLET <arthur.aillet@smile.fr>
2026-08-10 16:01:26 +0200

    feat: start of a calculator example

---
+++ examples/calculator/App.qml
@@ -0,0 +1,13 @@
+import QtQuick
+import QtQuick.Controls
+import NodeEditor
+
+ApplicationWindow {
+    width: 400
+    height: 400
+    visible: true
+    color: "black"
+    GraphicsView {
+        anchors.fill: parent
+    }
+}
---
+++ examples/calculator/main.cpp
@@ -0,0 +1,27 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include "DataFlowGraphModel.hpp"
+#include "DataFlowModelInterface.hpp"
+
+int main(int argc, char *argv[]) {
+  QCoreApplication::setOrganizationName("examples");
+  QCoreApplication::setApplicationName("Calculator");
+
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+      []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+
+  auto reg = std::make_shared<NodeDelegateModelRegistry>(&engine);
+
+  std::shared_ptr<DataFlowGraphModel> graph = std::make_shared<DataFlowGraphModel>(reg, &engine);
+
+  DataFlowModelInterface::init(graph.get());
+
+  engine.loadFromModule("examples.calculator", "App");
+
+  return app.exec();
+}
