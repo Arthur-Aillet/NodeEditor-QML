0dc774faf940707ce56aa3157e83e4036c14da3a
Arthur AILLET <arthur.aillet@smile.fr>
2026-08-10 12:02:43 +0200

    feat: removed anything cutie designer related, only keeps the NodeEditor

--- src/CutieDesigner/datatypes/SurfaceData.cpp
+++
@@ -1,4 +0,0 @@
-#include "SurfaceData.hpp"
-
-SurfaceData::SurfaceData(std::unique_ptr<QQmlComponent> &&object, QVariantMap initialProps)
-    : _component(std::move(object)), _initialProps(initialProps) {}
--- src/CutieDesigner/datatypes/Vec2Data.cpp
+++
@@ -1,9 +0,0 @@
-#include "Vec2Data.hpp"
-#include <qvectornd.h>
-
-Vec2DataType::Vec2DataType() : NodeDataType("vec2-01", "Vec2", {"vec2-01", "decimal"}) {}
-
-Vec2Data::Vec2Data(const QVector2D &v) {
-  registerConvert([&v]() { return v; });
-  registerConvert([&v]() { return (double)v.length(); });
-}
